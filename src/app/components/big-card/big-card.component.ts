525a62e56ef046140603d22c2111123fcf75c2ab
Perkles <bruna.camacho@unesp.br>
2023-05-28 18:00:04 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
+  photoCover: string =""
+  cardTitle:string =""
+  cardDescription: string =""
 
 }
